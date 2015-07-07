1bc38b2e0e5e96630a3c783d44a2146e8198ded9
sridhar <sridhar@path.com>
2015-07-07 15:25:45 -0700

    quick html out

--- scripts/check_mongo_config.py
+++ scripts/check_mongo_config.py
@@ -5,28 +5,29 @@ from distutils.version import StrictVersion
 import json
 import logging
 from pymongo import MongoClient
-import re
 
 from mongo_setup import MONITORING_DB, MONITORING_HOSTS, CONNECTION_TIMEOUT_MS
 
 
-logging.basicConfig(level='INFO', format='%(asctime)s %(levelname)s [%(name)s] %(message)s')
+logging.basicConfig(
+    level='INFO',
+    format='%(asctime)s %(levelname)s [%(name)s] %(message)s')
 log = logging.getLogger('check_mongo_config')
 
 
 def main(mongo_uri, minimum_version, output_file):
     conn = MongoClient(mongo_uri, connectTimeoutMS=CONNECTION_TIMEOUT_MS)
-    final_results = { 
-        'minimum_version' : minimum_version,
-        'results' : {}
+    final_results = {
+        'minimum_version': minimum_version,
+        'results': {}
     }
-    for seed_host in conn[MONITORING_DB][MONITORING_HOSTS].find().sort([('live', 1), 
-        ('_id', 1)]):
+    for seed_host in conn[MONITORING_DB][MONITORING_HOSTS].find().sort(
+        [('live', 1), ('_id', 1)]):
         results = {
-            'mongod' : [],
-            'mongos' : [],
-            'config' : [],
-            'errors' : []
+            'mongod': [],
+            'mongos': [],
+            'config': [],
+            'errors': []
         }
         label = seed_host['_id']
         live = seed_host.get('live', False)
@@ -45,31 +46,50 @@ def main(mongo_uri, minimum_version, output_file):
             for host in hosts:
                 process(host, minimum_version, results, skip_mongos)
         else:
-            log.warning('Skipping {0} as hosts incorrectly specified'.format(label))
+            log.warning('Skipping %s as hosts incorrectly specified' % label)
         final_results['results'][label] = results
-    with open(output_file, 'w') as fp:
+    with open(output_file+'.json', 'w') as fp:
         json.dump(final_results, fp)
+    write_html(output_file+'.html', final_results)
 
 
-def process(server_uri, minimum_version, results, skip_mongos=False, process_subs=True, process_override=None):
+def process(
+    server_uri,
+    minimum_version,
+    results,
+    skip_mongos=False,
+    process_subs=True,
+    process_override=None):
     log.debug('Processing {0} with subs {1}'.format(server_uri, process_subs))
     try:
-        conn = MongoClient(server_uri, connectTimeoutMS=CONNECTION_TIMEOUT_MS, slaveOk=True)
+        conn = MongoClient(
+            server_uri,
+            connectTimeoutMS=CONNECTION_TIMEOUT_MS,
+            slaveOk=True)
         log.debug('Obtained connection to {0}'.format(server_uri))
-        server_status = conn['admin'].command({'serverStatus' : 1, 'recordStats' : 0})
+        server_status = conn['admin'].command(
+            {'serverStatus': 1, 'recordStats': 0})
         process = server_status['process']
         version = server_status['version']
         if not process_subs or process == 'mongod':
             if process_override:
-                add_server_info(results, server_uri, process_override, version,
+                add_server_info(
+                    results,
+                    server_uri,
+                    process_override,
+                    version,
                     minimum_version)
             else:
-                add_server_info(results, server_uri, process, version,
+                add_server_info(
+                    results,
+                    server_uri,
+                    process,
+                    version,
                     minimum_version)
         else:
             process_sharded_cluster(results, conn, minimum_version, skip_mongos)
     except:
-        results['errors'].append({ 'server' : server_uri})
+        results['errors'].append({'server': server_uri})
 
 
 def get_valid_version(version, minimum_version):
@@ -97,23 +117,24 @@ def process_mongos(results, conn, minimum_version):
 
 def process_mongods(results, conn, minimum_version):
     log.info('In processing mongods for {0}'.format(conn))
-    shards = conn['config']['shards'].find({},{'_id':0, 'host':1})
+    shards = conn['config']['shards'].find({}, {'_id': 0, 'host': 1})
     for replica_set in shards:
         candidates = replica_set['host'].split('/')[-1]
         success = False
         replica_set_members = None
         for mongod in candidates.split(','):
-            try :
+            try:
                 replica_set_members = get_replica_set_members(mongod)
                 success = True
                 break
             except:
-                log.exception('Error getting replica set info for {0}'.format(mongod))
+                log.exception('Error getting replica set info for {0}'.format(
+                    mongod))
         if success:
             for member in replica_set_members:
                 process(member, minimum_version, results)
         else:
-            results['errors'].append({ 'server' : server_uri})
+            results['errors'].append({'server': mongod})
     log.info('Done processing mongods')
 
 
@@ -124,7 +145,11 @@ def process_configs(results, conn, minimum_version):
     for config_server in config_servers.split(','):
         if ":" not in config_server:
             config_server = config_server + ':27019'
-        process(config_server, minimum_version, results, process_override='config')
+        process(
+            config_server,
+            minimum_version,
+            results,
+            process_override='config')
     log.info('Done processing configs')
 
 
@@ -132,15 +157,15 @@ def add_server_info(results, server, process, version, minimum_version):
     if 'mongos' in process:
         process = 'mongos'
     results[process].append({
-        'server' : server, 
-        'version' : version,
-        'valid' : get_valid_version(version, minimum_version)
+        'server': server,
+        'version': version,
+        'valid': get_valid_version(version, minimum_version)
     })
 
 
-def get_replica_set_members(member):
-    log.info('Get replica set info for {0}'.format(member))
-    conn = MongoClient(member)
+def get_replica_set_members(node):
+    log.info('Get replica set info for {0}'.format(node))
+    conn = MongoClient(node)
     rsconfig = conn['local']['system.replset'].find_one()
     if rsconfig:
         return [member['host'] for member in rsconfig['members']]
@@ -148,12 +173,78 @@ def get_replica_set_members(member):
         return [member]
 
 
+def write_html(file_name, results):
+    with open(file_name, 'w') as out_file:
+        out_file.write('<html>\n')
+        out_file.write('\t<head>\n')
+        out_file.write('\t\t<title>Check Mongo Clusters</title>')
+        out_file.write("""
+        <style type="text/css">
+            .error {
+                background-color: white;
+                color: red;
+            }
+            .warning {
+                background-color: white;
+                color: orange;
+            }
+        </style>\n""")
+        out_file.write('\t</head>\n')
+        out_file.write('\t<body>\n')
+        out_file.write(
+            '\t\t<h1>Base version - %s</h1>\n' %
+            results['minimum_version'])
+        write_output_body(out_file, results['results'])
+        out_file.write('\t</body>\n')
+        out_file.write('</html>\n')
+        out_file.flush()
+
+
+def write_output_body(out_file, results):
+    for cluster_name, cluster_result in results.iteritems():
+        out_file.write('\n\t\t<h2>%s</h2>\n' % cluster_name)
+        out_file.write('\t\t<h3>Config servers</h3>\n')
+        write_table(out_file, cluster_result.get('config', []))
+        out_file.write('\t\t<h3>Data nodes</h3>\n')
+        write_table(out_file, cluster_result.get('mongod', []))
+        out_file.write('\t\t<h3>Mongos</h3>\n')
+        write_table(out_file, cluster_result.get('mongos', []))
+        out_file.write('\t\t<h3>Errors / unreachable</h3>\n')
+        out_file.write('\t\t<ul>\n')
+        for server in cluster_result.get('errors', []):
+            out_file.write(
+                '\t\t\t<li class="error">%s</li>\n' %
+                server['server'])
+        out_file.write('\t\t</ul>\n')
+
+
+def write_table(out_file, servers):
+    if not servers:
+        return
+    out_file.write('\t\t<table border="1">\n')
+    out_file.write('\t\t\t<tr><th>Server</th><th>Version</th></tr>\n')
+    for server in servers:
+        if server['valid']:
+            out_file.write(
+                '\t\t\t<tr><td>%s</td><td>%s</td></tr>\n' %
+                (server['server'], server['version']))
+        else:
+            out_file.write(
+                '\t\t\t<tr><td>%s</td><td class="warning">%s</td></tr>\n' %
+                (server['server'], server['version']))
+    out_file.write('\t\t</table>\n')
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('mongo_uri', help='Mongo(d/s) URI')
-    parser.add_argument('--minimum_version',
-        help = 'Minimum 3 digit version to check', default='2.0.0')
-    parser.add_argument('--output_file',
-        help = 'Output file', default='mongo_check.json')
+    parser.add_argument(
+        '--minimum_version',
+        help='Minimum 3 digit version to check',
+        default='2.0.0')
+    parser.add_argument(
+        '--output_file',
+        help='Output file',
+        default='mongo_check.json')
     args = parser.parse_args()
     main(args.mongo_uri, args.minimum_version, args.output_file)
